cb48bd8e924ec9003c960e51b974a2d73ea93bf0
Parvez Anandam <parvezanandam@gmail.com>
2011-08-07 21:36:16 -0700

    The SeattleVue app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SeattleVue::Application.config.secret_token = '45ab3831f137775a12b3b2a54009033afd84cf43acfb22a356e7dc069500bac33045fed0de3ce3f87d81bbdce61f63e2a389d02267788fd3751efd4fa7964a9f'
